fd1cd847187d4d070b260566a28d415d1910c217
Michael Smith <mike@spokefire.co.uk>
2014-10-28 19:07:57 +0000

    Allow the listening port to be customised

--- whatismyip.go
+++ whatismyip.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"net/http"
 )
+
+var port = flag.Int("port", 8080, "Port to use, defaults to 8080")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ipAddress, _, _ := net.SplitHostPort(r.RemoteAddr)
@@ -12,6 +15,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	flag.Parse()
+
+	hostAndPort := fmt.Sprintf(":%d", *port)
+
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(hostAndPort, nil)
 }
